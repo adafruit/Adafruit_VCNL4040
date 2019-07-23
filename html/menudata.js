75f530948acf9fc0fc0dfa3579aa8476c4407fb0
Travis CI <travis@travis-ci.org>
2019-07-23 01:23:57 +0000

    Deploy code docs to GitHub Pages Travis build: 12
    Commit: 28a2617f45130b97cfe37fe7245f02543bc4ca7d

--- html/menudata.js
+++ html/menudata.js
@@ -4,7 +4,13 @@ var menudata={children:[
 {text:"Class List",url:"annotated.html"},
 {text:"Class Index",url:"classes.html"},
 {text:"Class Members",url:"functions.html",children:[
-{text:"All",url:"functions.html"},
+{text:"All",url:"functions.html",children:[
+{text:"a",url:"functions.html#index_a"},
+{text:"b",url:"functions.html#index_b"},
+{text:"e",url:"functions.html#index_e"},
+{text:"g",url:"functions.html#index_g"},
+{text:"p",url:"functions.html#index_p"},
+{text:"s",url:"functions.html#index_s"}]},
 {text:"Functions",url:"functions_func.html"},
 {text:"Variables",url:"functions_vars.html"}]}]},
 {text:"Files",url:"files.html",children:[
--- html/search/all_3.js
+++ html/search/all_3.js
@@ -1,9 +1,16 @@
 var searchData=
 [
+  ['getambientintegrationtime',['getAmbientIntegrationTime',['../class_adafruit___v_c_n_l4040.html#a957b615cfcdb86c65830bb25aba3176b',1,'Adafruit_VCNL4040']]],
   ['getambientlight',['getAmbientLight',['../class_adafruit___v_c_n_l4040.html#ae72c95023e5c5d38514e4f9b770d57bf',1,'Adafruit_VCNL4040']]],
   ['getambientlighthighthreshold',['getAmbientLightHighThreshold',['../class_adafruit___v_c_n_l4040.html#abc6a4ebc7158b9f4090f8876d3f80864',1,'Adafruit_VCNL4040']]],
   ['getambientlightlowthreshold',['getAmbientLightLowThreshold',['../class_adafruit___v_c_n_l4040.html#aaeae368d3727d967446cef4e8e5740f1',1,'Adafruit_VCNL4040']]],
   ['getinterruptstatus',['getInterruptStatus',['../class_adafruit___v_c_n_l4040.html#a5ec2ecad8ab1722dc68e81ea3c5387bd',1,'Adafruit_VCNL4040']]],
   ['getproximity',['getProximity',['../class_adafruit___v_c_n_l4040.html#accdc33d3cc800395932f40ff7979b40d',1,'Adafruit_VCNL4040']]],
+  ['getproximityhighresolution',['getProximityHighResolution',['../class_adafruit___v_c_n_l4040.html#ae8f197747d8625b970dc9cd3679486d4',1,'Adafruit_VCNL4040']]],
+  ['getproximityhighthreshold',['getProximityHighThreshold',['../class_adafruit___v_c_n_l4040.html#a4062bbc0291dd20f15608758fa8cb971',1,'Adafruit_VCNL4040']]],
+  ['getproximityintegrationtime',['getProximityIntegrationTime',['../class_adafruit___v_c_n_l4040.html#ac54579f49a7ac7625d5f44c2a81177c6',1,'Adafruit_VCNL4040']]],
+  ['getproximityledcurrent',['getProximityLEDCurrent',['../class_adafruit___v_c_n_l4040.html#afcf571dd841d9d0715018eb078eb3ff9',1,'Adafruit_VCNL4040']]],
+  ['getproximityleddutycycle',['getProximityLEDDutyCycle',['../class_adafruit___v_c_n_l4040.html#a73d65716a83ea202a87811eb5b9b67c0',1,'Adafruit_VCNL4040']]],
+  ['getproximitylowthreshold',['getProximityLowThreshold',['../class_adafruit___v_c_n_l4040.html#a06f91ee7bdfb62a7028980c81c1c006c',1,'Adafruit_VCNL4040']]],
   ['getwhitelight',['getWhiteLight',['../class_adafruit___v_c_n_l4040.html#aac6d624ff54971d00ac12c44f92eb781',1,'Adafruit_VCNL4040']]]
 ];
